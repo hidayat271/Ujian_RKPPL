c66e91f5f4d5353595a1c0dbdbb51b2a188edfe5
Hidayat <hidayatpey@mail.unpas.ac.id>
2014-05-21 15:42:06 +0700

    Base Project + Test Case pangkat

---
+++ src/test/java/com/test/Soal.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.test;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Apey
+ */
+public class Soal {
+    
+    public Soal() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    @Before
+    public void soal(){
+        soal1 = new com.ujian.ujian_rkppl.Soal();
+    }
+    
+    private static com.ujian.ujian_rkppl.Soal soal1;
+    
+    @Test
+    public void tesPangkat(){
+        soal1.pangkat(2, 2);
+        soal1.pangkat(0, 1);
+    }
+    
+    
+}
